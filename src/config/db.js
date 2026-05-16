66d4e97fef453399d268e7f1f06f52735ddae13d
ribhupramanik <ribhu02@gmail.com>
2026-05-16 10:38:24 +0530

    Added .env example and modified db.js to use tidb database

--- src/config/db.js
+++ src/config/db.js
@@ -1,26 +1,68 @@
-import mysql from "mysql2";
+// Uncomment the code below for local testing
+
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+// connection.getConnection((err, conn) => {
+//     if (err) {
+//         console.log("Database connection failed:", err.message);
+//     } else {
+//         console.log("MySQL Connected Successfully");
+//         conn.release();
+//     }
+// });
+
+// export default connection.promise();
+
+
+// Comment the code below for local testing
+
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createPool({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    connectionLimit: 10
 });
 
-connection.getConnection((err, conn) => {
-    if (err) {
-        console.log("Database connection failed:", err.message);
-    } else {
-        console.log("MySQL Connected Successfully");
-        conn.release();
-    }
-});
+try {
 
-export default connection.promise();
+    const conn = await db.getConnection();
+
+    console.log("TiDB Connected Successfully");
+
+    conn.release();
+
+} catch (error) {
+
+    console.log("Database Error:", error.message);
+
+}
+
+export default db;
